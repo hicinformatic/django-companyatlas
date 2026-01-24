268aea579d81d74a27a8f842bed2c82c6e059c1e
Charles Delencr <hicinformatic@gmail.com>
2026-01-24 18:52:40 +0100

    refactor: improve CompanyAtlasVirtualCompany model using define_fields_from_config decorator
    - Refactor model to use VirtualModel base class
    - Use define_fields_from_config decorator from django-providerkit
    - Add fields_associations to djcompanyatlas __init__
    - Generate fields dynamically from COMPANYATLAS_SEARCH_COMPANY_FIELDS
    - Add companyatlas_id as primary key field

--- src/djcompanyatlas/__init__.py
+++ src/djcompanyatlas/__init__.py
@@ -5,7 +5,23 @@
 #   from djcompanyatlas.models import Company, CompanyData, CompanyDocument, CompanyEvent
 #   from djcompanyatlas.helpers import create_or_update_company_data, ...
 
+from django.db import models
+
 __all__ = []
 
 # Version
 __version__ = "0.1.0"
+
+fields_associations = {
+    'int': models.IntegerField,
+    'float': models.FloatField,
+    'bool': models.BooleanField,
+    'list': models.JSONField,
+    'str': models.CharField,
+    'text': models.TextField,
+    'date': models.DateField,
+    'time': models.TimeField,
+    'datetime': models.DateTimeField,
+    'email': models.EmailField,
+    'url': models.URLField,
+}
--- src/djcompanyatlas/models/virtuals/company.py
+++ src/djcompanyatlas/models/virtuals/company.py
@@ -1,17 +1,52 @@
+"""Company suggestion model for companyatlas."""
+
+from typing import Any
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from companyatlas import COMPANYATLAS_SEARCH_COMPANY_FIELDS
+from djproviderkit.models.service import define_fields_from_config
+from virtualqueryset.models import VirtualModel
 
-from ...managers.virtuals.company import CompanyAtlasVirtualCompanyManager
+from djcompanyatlas.managers.virtuals.company import CompanyAtlasVirtualCompanyManager
+
+FIELDS_COMPANYATLAS = COMPANYATLAS_SEARCH_COMPANY_FIELDS
+
+companyatlas_id_config: dict[str, Any] = FIELDS_COMPANYATLAS['companyatlas_id']
 
 
-class CompanyAtlasVirtualCompany(models.Model):
-    """Virtual company model for companyatlas."""
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+@define_fields_from_config(FIELDS_COMPANYATLAS, primary_key='companyatlas_id')
+class BaseCompanyAtlasVirtualCompany(VirtualModel):
+    """Virtual model for company suggestions from companyatlas."""
+
+    companyatlas_id: models.CharField = models.CharField(
+        max_length=500,
+        verbose_name=companyatlas_id_config['label'],
+        help_text=companyatlas_id_config['description'],
+        primary_key=True,
+    )
+
     objects = CompanyAtlasVirtualCompanyManager()
 
     class Meta:
         managed = False
-        verbose_name = _("Virtual Company")
-        verbose_name_plural = _("Virtual Companies")
+        abstract = True
+        verbose_name = _('Company Suggestion')
+        verbose_name_plural = _('Company Suggestions')
+
+    def __str__(self) -> str:
+        denomination = getattr(self, 'denomination', None)
+        companyatlas_id = getattr(self, 'companyatlas_id', None)
+        if denomination:
+            return str(denomination)
+        return f"Company {companyatlas_id or 'unknown'}"
+
+
+class CompanyAtlasVirtualCompany(BaseCompanyAtlasVirtualCompany):
+    """Model for companyatlas companies."""
+
+    class Meta:
+        managed = False
+        verbose_name = _('CompanyAtlas Company')
+        verbose_name_plural = _('CompanyAtlas Companies')
 
